def769ba1d185e80c94ea723919246d32d27813e
pymba86 <pymba-96@mail.ru>
2025-06-28 00:17:19 +0500

    555

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import About from './pages/About.vue'
 import App from './App.vue'
 
 export const createApp = ViteSSG(App,{
+    base: '/',
   routes: [
     { path: '/', component: Home },
     { path: '/about', component: About }
