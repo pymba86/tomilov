c6b2ebbe31c05f6d12aac18db64e22eea037956c
Artem Matveychuk <pymba-96@mail.ru>
2025-07-01 18:11:17 +0500

    htmkl

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,21 @@
-
 <template>
-  <main>
-    <RouterView />
-  </main>
+    <main>
+        <RouterView />
+    </main>
 </template>
+
+<style>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+body {
+    font-family: "Inter", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+        sans-serif;
+    line-height: 1.6;
+    color: #333;
+    overflow-x: hidden;
+}
+</style>
