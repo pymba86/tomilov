043aaa5a8f2264cb066356d045ef742388882d03
Artem Matveychuk <pymba-96@mail.ru>
2025-07-01 18:07:54 +0500

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
-import { ViteSSG } from 'vite-ssg'
-import { createWebHistory } from 'vue-router'
-import Home from './pages/Home.vue'
-import About from './pages/About.vue'
-import App from './App.vue'
+import { ViteSSG } from "vite-ssg";
+import Home from "./pages/Home.vue";
+import About from "./pages/About.vue";
+import App from "./App.vue";
 
-export const createApp = ViteSSG(App,{
-    base: '/',
-  routes: [
-    { path: '/', component: Home },
-    { path: '/about', component: About }
-  ],
-})
+export const createApp = ViteSSG(App, {
+    base: "/",
+    routes: [
+        { path: "/", component: Home },
+        { path: "/about", component: About },
+    ],
+});
--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,8 +1,6 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 <template>
-  <div>
-    <h1>О нас</h1>
-    <p><router-link to="/">Вернуться на главную</router-link></p>
-  </div>
+    <div>
+        <p><router-link to="/">Вернуться на главную</router-link></p>
+    </div>
 </template>
