6c8e3960d6cc75ce08f543960652382fdf3b55bf
Artem Matveychuk <pymba-96@mail.ru>
2025-07-01 18:50:16 +0500

    fix icon

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -196,6 +196,9 @@ import {
     MessageCircle as MessageCircleIcon,
     Instagram as InstagramIcon,
     Users as UsersIcon,
+    MusicIcon,
+    CheckIcon,
+    CalendarIcon,
 } from "lucide-vue-next";
 
 // Reactive data
@@ -213,39 +216,21 @@ const services = ref([
         id: 2,
         title: "Подбор музыки",
         description: "И стиля под тематику свадьбы",
-        icon: {
-            template: `
-    <svg fill="currentColor" viewBox="0 0 24 24">
-      <path d="M12 3v10.55c-.59-.34-1.27-.55-2-.55-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4V7h4V3h-6z" />
-    </svg>
-  `,
-        },
+        icon: MusicIcon,
         iconClass: "iconPurple",
     },
     {
         id: 3,
         title: "Участие молодоженов",
         description: "Возможность участия в номере жениха и невесты",
-        icon: {
-            template: `
-    <svg fill="currentColor" viewBox="0 0 24 24">
-      <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z" />
-    </svg>
-  `,
-        },
+        icon: CheckIcon,
         iconClass: "iconGreen",
     },
     {
         id: 4,
         title: "Полный спектр услуг",
         description: "От идеи до реализации",
-        icon: {
-            template: `
-    <svg fill="currentColor" viewBox="0 0 24 24">
-      <path d="M9 11H7v6h2v-6zm4 0h-2v6h2v-6zm4 0h-2v6h2v-6zm2.5-9H19V1h-2v1H7V1H5v1H4.5C3.67 2 3 2.67 3 3.5v15C3 19.33 3.67 20 4.5 20h15c.83 0 1.5-.67 1.5-1.5v-15C21 2.67 20.33 2 19.5 2z" />
-    </svg>
-  `,
-        },
+        icon: CalendarIcon,
         iconClass: "iconOrange",
     },
 ]);
@@ -299,13 +284,7 @@ const contacts = ref([
         title: "VK",
         text: "Профиль",
         href: "https://vk.com/id393654792",
-        icon: {
-            template: `
-    <svg fill="currentColor" viewBox="0 0 24 24">
-      <path d="M15.684 0H8.316C1.592 0 0 1.592 0 8.316v7.368C0 22.408 1.592 24 8.316 24h7.368C22.408 24 24 22.408 24 15.684V8.316C24 1.592 22.408 0 15.684 0zm3.692 17.123h-1.744c-.66 0-.864-.525-2.05-1.727-1.033-1.01-1.49-1.135-1.744-1.135-.356 0-.458.102-.458.593v1.575c0 .424-.135.678-1.253.678-1.846 0-3.896-1.118-5.335-3.202C4.624 10.857 4.03 8.57 4.03 7.348c0-.254.102-.491.593-.491 1.068 0 1.491.763 1.915 1.694.458.966 1.017 2.098 1.694 2.098.254 0 .356-.118.356-.763V8.348c-.085-1.305-.763-1.422-.763-1.896 0-.203.169-.407.441-.407h2.729c.678 0 .932.347.932 1.388v3.659c0 .678.305.932.763.932.254 0 .695-.118 1.727-1.135 1.016-1.017 1.744-2.59 1.744-2.59.102-.254.356-.491.763-.491h1.744c1.017 0 1.253.525.932 1.237-.458 1.152-2.138 3.235-2.138 3.235-.254.356-.356.525 0 .932.254.305 1.084 1.068 1.643 1.727.932 1.118.525 1.694-.407 1.694z" />
-    </svg>
-  `,
-        },
+        icon: UsersIcon,
         external: true,
     },
     {
